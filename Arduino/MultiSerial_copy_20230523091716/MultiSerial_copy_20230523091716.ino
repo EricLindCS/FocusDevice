9b17d1ec99940b70b028bcc293c670e2a881d63a
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2023-05-23 09:30:12 -0700

    OdeUpdate

--- Arduino/MultiSerial_copy_20230523091716/MultiSerial_copy_20230523091716.ino
+++ Arduino/MultiSerial_copy_20230523091716/MultiSerial_copy_20230523091716.ino
@@ -1,18 +1,22 @@
-const int ledPin = 13;
+int ledPin1 = 2;  // Pin connected to LED 1
+int ledPin2 = 3;  // Pin connected to LED 2
+int ledPin3 = 4;  // Pin connected to LED 3
 
 void setup() {
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    int value = Serial.parseInt();
-    if (value > 100) {
-      digitalWrite(ledPin, HIGH);
-    } else {
-      digitalWrite(ledPin, LOW);
-    }
-    Serial.println(value); // Echo the received value back to the computer
+  if (Serial.available() >= 3) {
+    int value1 = Serial.parseInt();
+    int value2 = Serial.parseInt();
+    int value3 = Serial.parseInt();
+
+    analogWrite(ledPin1, value1);
+    analogWrite(ledPin2, value2);
+    analogWrite(ledPin3, value3);
   }
 }
