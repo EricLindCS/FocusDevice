e64182d276f1e4edec4606be0ecca9219ae1dbbb
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2023-05-30 12:15:58 -0700

    Webint V2

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,35 @@
+
+
 const express = require('express');
+const SerialPort = require('serialport').ReadlineParser;
+
 const app = express();
-const port = 3000; // Change the port number if needed
+const port = 3000;
 
-// Serve the HTML file
-app.use(express.static('public'));
+const arduinoPort = new SerialPort({
+    path: 'COM3',
+    baudRate: 9600,
+});
 
-// Start the server
+app.get('/', (req, res) => {
+  res.send('Web Interface');
+});
+
+app.get('/send/:value', (req, res) => {
+  const value = parseInt(req.params.value);
+
+  // Send the value to the Arduino over the serial connection
+  arduinoPort.write(value.toString(), (err) => {
+    if (err) {
+      console.error('Error sending data:', err);
+      res.status(500).send('Error sending data');
+    } else {
+      console.log('Data sent:', value);
+      res.send('Data sent');
+    }
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server listening on port ${port}`);
 });
